1eb64d1ff4ef23475b8784a5097c16c47832fddf
Ana Teperman <tepermana@udesa.edu.ar>
2021-11-15 12:03:19 -0300

    primer fetch del index para películas populares

---
+++ javascript/index.js
@@ -0,0 +1,30 @@
+// apikey c0945689b0a582e110971301d6ea8be2
+window.addEventListener('load', function(){
+fetch (`https://api.themoviedb.org/3/movie/popular?api_key=c0945689b0a582e110971301d6ea8be2`)
+    .then(function(response){
+        return response.json();
+    })
+    .then (function(datos){
+
+        console.log (datos.results);
+        
+        for(let i = 0; i< 5; i++){
+            
+        document.querySelector('.contenedor').innerHTML += `
+        <div class="hijo">
+        <div class="imagen-port">
+            <a href="./detail-movies.html"><img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datos.results[i].poster_path}" alt="ficha ${datos.results[i].title}"></a>
+            <h3>${datos.results[i].title}</h3>
+            <p>${datos.results[i].release_date}</p>
+        </div>
+    </div>
+        `
+        }
+    })
+    .catch(function (error) {
+        console.log(`El error fue: ${error}`);
+    })
+
+
+
+})
