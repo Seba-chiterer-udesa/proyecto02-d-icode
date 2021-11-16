2fc28c501a2c4a563482aafd672fdf08d1221c64
TobiasDenhamDH <tdenham@udesa.edu.ar>
2021-11-15 22:19:22 -0300

    validacion terminada y vinculacion interna con js

--- js/validacion.js
+++ js/validacion.js
@@ -5,13 +5,19 @@ window.addEventListener("load",function(){
 let formulario = document.querySelector ("form");
 let campoBuscar = document.querySelector ("[name=busqueda]");
 let mensaje = document.querySelector(".alert");
+let busqueda = document.querySelector(".header-busqueda input");
      
 formulario.addEventListener("submit",function(e){
     e.preventDefault();
     if(campoBuscar.value == ""){   
-        mensaje.innerText = 'Busqueda vacía';
+        mensaje.innerText = 'Búsqueda vacía';
+        busqueda.style.marginBottom = '5px';
+        mensaje.style.marginBottom = '12px';
     } else if (campoBuscar.value.length < 3){ 
         mensaje.innerText = 'Ingrese al menos 3 caracteres';
+        busqueda.style.marginBottom = '5px';
+        mensaje.style.marginBottom = '12px';
+        mensaje.style.fontSize = '11px';
     } else {
         this.submit();
 }})
