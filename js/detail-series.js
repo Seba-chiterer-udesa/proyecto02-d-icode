617770b0e314f120a809e1e49d9d05e7fe15b189
Ana Teperman <tepermana@udesa.edu.ar>
2021-11-17 09:54:19 -0300

    detail series casi listo

--- js/detail-series.js
+++ js/detail-series.js
@@ -11,6 +11,12 @@ fetch(`https://api.themoviedb.org/3/tv/${idSerie}?api_key=c0945689b0a582e1109713
 })
 .then(function(datos) {
     console.log(datos);
+    
+    let generos =""
+    for (let i =0; i < datos.genres.length; i++ ){
+        generos += `<a href="detail-genres.html?idGenero=${datos.genres[i].id}">${datos.genres[i].name} </a>`
+    }
+    console.log(generos)
         document.querySelector('.titulo').innerHTML += `
             <h2>• ${datos.name}• </h2>
         `
@@ -19,12 +25,12 @@ fetch(`https://api.themoviedb.org/3/tv/${idSerie}?api_key=c0945689b0a582e1109713
             <div class="imagen-port series">
                 <img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datos.poster_path}" alt="ficha ${datos.title}">
                 <h3>${datos.name}</h3>
-                <p>${datos.genres}</p>
+                <p>${generos}</p>
                 <p>${datos.first_air_date } ‧ ${datos.number_of_seasons}</p>
             </div>
             <div class="detailseriessinopsis-container">  
                 <h2>${datos.name}</h2>                     
-                <h3> Género: ${datos.genres}</h3>
+                <h3> Género: ${generos}</h3>
                 <h3>Clasificación: ${datos.type}</h3>
                 <h3>Sinópsis: ${datos.overview}</h3>
                 <h3>Calificación promedio: ${datos.vote_average}</h3>
