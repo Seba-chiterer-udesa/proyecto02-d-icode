eb0f1873abaadfc5fd34e4acf82c86af4fd1f2ed
TobiasDenhamDH <tdenham@udesa.edu.ar>
2021-11-17 10:12:14 -0300

    arreglando validacion en todas las paginas

--- js/index.js
+++ js/index.js
@@ -8,17 +8,21 @@ fetch (`https://api.themoviedb.org/3/movie/popular?api_key=c0945689b0a582e110971
 
         
         for(let i = 0; i< 5; i++){
+
+            for(let genre_ids=0;genre_ids<this.lenght;genre_ids++){
+                document.querySelector('.contenedor').innerHTML += `
+                <div class="hijo">
+                    <div class="imagen-port">
+                        <a href="./detail-movies.html"><img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datos.results[i].poster_path}" alt="ficha ${datos.results[i].title}"></a>
+                        <h3>${datos.results[i].title}</h3>
+                        <p>${datos.results[i].genre_ids}</p>
+                    </div>
+                </div>
+                `
+                }
+            }
             
-        document.querySelector('.contenedor').innerHTML += `
-        <div class="hijo">
-            <div class="imagen-port">
-                <a href="./detail-movies.html"><img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datos.results[i].poster_path}" alt="ficha ${datos.results[i].title}"></a>
-                <h3>${datos.results[i].title}</h3>
-                <p>${datos.results[i].genre_id}</p>
-            </div>
-        </div>
-        `
-        }
+       
         console.log(datos);
     })
     .catch(function (error) {
