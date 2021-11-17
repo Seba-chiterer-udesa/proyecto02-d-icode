3ea4008bfc1680389e8b0d689be74383e9d701f5
Ana Teperman <tepermana@udesa.edu.ar>
2021-11-17 09:19:52 -0300

    detail series

--- js/detail-series.js
+++ js/detail-series.js
@@ -11,31 +11,34 @@ fetch(`https://api.themoviedb.org/3/tv/${idSerie}?api_key=c0945689b0a582e1109713
 })
 .then(function(datos) {
     console.log(datos);
-        document.querySelector('.titulo').innerHTML = `
-            <h2>• ${datos.results[idSerie].title}• </h2>
+        document.querySelector('.titulo').innerHTML += `
+            <h2>• ${datos.name}• </h2>
         `
-        document.querySelectorAll('.contenedorSerie').innerHTML = `
-        <div class="hijo-detail-series">
+        document.querySelector('.hijo-detail-series').innerHTML += `
+        
             <div class="imagen-port series">
-                <img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datos.results[idSerie].poster_path}" alt="ficha ${datos.results[idSerie].title}">
-                <h3>${datos.results[idSerie].title}</h3>
-                <p>${datos.results[idSerie].genres}</p>
-                <p>${datos.results[idSerie].first_air_date } ‧ ${datos.results[idSerie].next_episode_to_air.number_of_seasons}</p>
+                <img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datos.poster_path}" alt="ficha ${datos.title}">
+                <h3>${datos.name}</h3>
+                <p>${datos.genres}</p>
+                <p>${datos.first_air_date } ‧ ${datos.number_of_seasons}</p>
             </div>
-            <div class="detailseriessinopsis-container">                       
-                <h3> Género: ${datos[idSerie].genres}</h3>
-                <h3>Clasificación: ${datos.results[idSerie].type}</h3>
-                <h3>Sinópsis: ${datos.results[idSerie].overview}</h3>
-                <h3>Calificación promedio: ${datos.results[idSeris].vote_average}</h3>
-                <h3>Total de Reseñas:${datos.results[idSeris].vote_count}</h3>
+            <div class="detailseriessinopsis-container">  
+                <h2>${datos.name}</h2>                     
+                <h3> Género: ${datos.genres}</h3>
+                <h3>Clasificación: ${datos.type}</h3>
+                <h3>Sinópsis: ${datos.overview}</h3>
+                <h3>Calificación promedio: ${datos.vote_average}</h3>
+
+                <h3>Total de Reseñas:${datos.vote_count}</h3>
             <div>
+
             <div class="boton-favoritos">
                     <div class="detail-favoritos">
                         <i class="fas fa-star star"></i>
                         <a href="./favourite.html"><h3>Agregar a Favoritos</h3></a>   
                     </div>
             </div>   
-        </div>
+       
         `
 
 })
