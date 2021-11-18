fc7a7f1d384eae5ad2c68fae1525769ba5cf2030
Ana Teperman <tepermana@udesa.edu.ar>
2021-11-18 19:03:04 -0300

    estructura fetch para el detailGereno

--- js/detailGenero.js
+++ js/detailGenero.js
@@ -2,10 +2,20 @@ window.addEventListener("load",function(){
 // ACCEDO A LA QUERY STRING
 let queryString = location.search;
 let objetoQueryString = new URLSearchParams(queryString);
-// console.log(objetoQueryString.get('idSerie'))
-let idSerie = objetoQueryString.get('idGenero');
-//FETCH
 
+let idGenero = objetoQueryString.get('idGenero');
+console.log(objetoQueryString.get('idGenero'))
+//FETCH
+fetch(`https://api.themoviedb.org/3/${idGenero}/tv/list?api_key=c0945689b0a582e110971301d6ea8be2`)
+.then(function(response){
+    return response.json();
+})
+
+.then()
+
+.catch(function (error) {
+    console.log(`El error fue: ${error}`);
+})
 
 
 })
