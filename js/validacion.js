b96795d5c8c17c6f6520ab9252ddc5c47883864c
TobiasDenhamDH <tdenham@udesa.edu.ar>
2021-11-19 09:30:05 -0300

    validacion terminada y arrgelos de esilo

--- js/validacion.js
+++ js/validacion.js
@@ -6,9 +6,10 @@ let formulario = document.querySelector ("form");
 let campoBuscar = document.querySelector ("[name=busqueda]");
 let mensaje = document.querySelector(".alert");
 let busqueda = document.querySelector(".header-busqueda input");
+let lupaBusqueda = document.querySelector(".lupa");
      
 formulario.addEventListener("submit",function(e){
-    e.preventDefault();
+    e.preventDefault(); 
     if(campoBuscar.value == ""){   
         mensaje.innerText = 'Búsqueda vacía';
         busqueda.style.marginBottom = '5px';
@@ -25,5 +26,20 @@ formulario.addEventListener("submit",function(e){
 campoBuscar.addEventListener('input', function(){
     mensaje.innerText = "";
 })
-     
+
+lupaBusqueda.addEventListener("click",function(e){
+    e.preventDefault(); 
+    if(campoBuscar.value == ""){   
+        mensaje.innerText = 'Búsqueda vacía';
+        busqueda.style.marginBottom = '5px';
+        mensaje.style.marginBottom = '12px';
+    } else if (campoBuscar.value.length < 3){ 
+        mensaje.innerText = 'Ingrese al menos 3 caracteres';
+        busqueda.style.marginBottom = '5px';
+        mensaje.style.marginBottom = '12px';
+        mensaje.style.fontSize = '11px';
+    } else {
+        this.submit();
+}})
+
 })
